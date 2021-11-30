e6017c4f62241f779d78593c89bfeb7322623371
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-11-30 12:10:00 -0500

    Import chiffrage par methode

--- src/index.js
+++ src/index.js
@@ -6,6 +6,31 @@ import formatteurMessage from './formatteurMessage.js'
 import forgecommon from './forgecommon.js'
 import cryptoForge from './cryptoForge.js'
 import constantes from './constantes.js'
-import * as chiffrage from './chiffrage.js'
+import {
+  detecterSubtle,
+  getRandomValues,
+  chiffrer, dechiffrer,
+  chiffrerForge, dechiffrerForge, chiffrerSubtle, dechiffrerSubtle,
+  creerCipher, creerDecipher,
+  chiffrerCleSecreteSubtle, dechiffrerCleSecreteSubtle,
+  importerClePubliqueSubtle, importerClePriveeSubtle,
+  chiffrerCleSecreteForge, dechiffrerCleSecreteForge,
+  chiffrerDocument, dechiffrerDocument,
+  preparerCommandeMaitrecles, dechiffrerDocumentAvecMq,
+  preparerCleSecreteSubtle,
+} from './chiffrage.js'
+export {
+    detecterSubtle,
+    getRandomValues,
+    chiffrer, dechiffrer,
+    chiffrerForge, dechiffrerForge, chiffrerSubtle, dechiffrerSubtle,
+    creerCipher, creerDecipher,
+    chiffrerCleSecreteSubtle, dechiffrerCleSecreteSubtle,
+    importerClePubliqueSubtle, importerClePriveeSubtle,
+    chiffrerCleSecreteForge, dechiffrerCleSecreteForge,
+    chiffrerDocument, dechiffrerDocument,
+    preparerCommandeMaitrecles, dechiffrerDocumentAvecMq,
+    preparerCleSecreteSubtle,
+}
 
-export { chiffrage, constantes, cryptoForge, forgecommon, formatteurMessage, hachage, idmg, validateurMessage }
+export { constantes, cryptoForge, forgecommon, formatteurMessage, hachage, idmg, validateurMessage }
