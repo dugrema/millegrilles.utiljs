d7bdf61532ba6655d19c77a26ff7c97f45fa4a45
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-01-08 17:04:11 -0500

    Changement export idmg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import hachage from './hachage.js'
 import validateurMessage from './validateurMessage.js'
 // import chiffrage from './chiffrage.js'
-import idmg from './idmg.js'
+//import idmg from './idmg.js'
 import formatteurMessage from './formatteurMessage.js'
 import forgecommon from './forgecommon.js'
 import constantes from './constantes.js'
 
-export { constantes, forgecommon, formatteurMessage, hachage, idmg, validateurMessage }
+export { constantes, forgecommon, formatteurMessage, hachage, validateurMessage }
 
 export * from './chiffrage.js'
 export * from './certificats'
+export * from './idmg.js'
